be15d7350963aebf1729b9fcb5dad9e19bcdd869
Jerôme Villiseck <villiseck@levant.abes.fr>
2019-03-18 09:55:22 +0100

    abstract class working add notes

--- java/src/main/java/object_oriented/e_abstract_and_interface/abstract_class/Felin.java
+++ java/src/main/java/object_oriented/e_abstract_and_interface/abstract_class/Felin.java
@@ -6,4 +6,8 @@ public abstract class Felin extends Animal {
     } //Comportement commun à chaque references felin
 
     abstract void crier(); //Comportement encore plus specifique a felin
+    /*IMPORTANT dans une classe de niveau intermediaire qui n'est pas censée etre
+    instanciée, toujours reporter les methodes abstraites de sa classe mère pour gagner en
+    visibilité de qui n'a pas encore été défini et reste a definir plus bas
+     */
 }
